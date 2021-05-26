7095545733568498a58e552c2643750c8eaf23a9
alx-b <>
2021-05-26 12:58:04 +0200

    Updating About and Home views.

--- src/views/AboutView.tsx
+++ src/views/AboutView.tsx
@@ -1,7 +1,10 @@
+import CityInfo from "../components/city_info/cityInfo"
+
 export const AboutView = () => {
     return (
         <div>
             <h1>About View!</h1>
+            <CityInfo />
         </div>
     )
 }
--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,11 +1,13 @@
 import WeatherCard from "../components/weather_card/WeatherCard"
+import SearchedLocationInput from "../components/searched_location_input/searchedLocationInput"
 
 export const HomeView = () => {
     return (
         <div>
-            <h1>Home View!</h1>
+            <h1>Today</h1>
             <div className="weather-cards">
                 <WeatherCard />
+                <SearchedLocationInput />
             </div>
         </div>
     )
